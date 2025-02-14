75f9a791082cc2eea0f125351e58789dbf7621d9
natanael <nathaxx09@hotmail.com>
2025-02-13 22:10:03 -0300

    primer commit - prueba de medida movil

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+/* const text = document.getElementById('myText');
+gsap.to(text, {
+    duration: 1.7,
+    text: "Web Developer",
+    ease: "none",
+});
+
+ScrollReveal().reveal('h1', {
+    delay: 0,
+    origin: 'left',
+    duration: 500,
+    distance: '100%'
+}); */
+ScrollReveal().reveal('.hero-logo', {
+    delay: 200,
+    origin: 'center',
+    duration: 3000,
+    distance: '120%'
+});
