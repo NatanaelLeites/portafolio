effcc7669f413b05aeb63413dd04c77ee094529e
natanael <nathaxx09@hotmail.com>
2025-03-03 19:10:47 -0300

    cambio de formato a descktop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import './menu-movil.js';
+const text = document.getElementById('myText');
+
+gsap.to(text, {
+    duration: 2.3,
+    text: "Desarrollador Web",
+    ease: "none",
+});
--- js/menu-movil.js
+++
@@ -1,9 +0,0 @@
-const btnMenu = document.getElementById('btnMenu')
-const menu = document.getElementById('menu')
-
-btnMenu.addEventListener('click', () =>{
-    btnMenu.classList.toggle('abierto')
-    menu.classList.toggle('activo')
-    const main = document.querySelector('.main')
-    main.classList.toggle('desplazamineto')
-})
