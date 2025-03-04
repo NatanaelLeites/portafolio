f585d7550dd1e06a7d80ef98ef40b8cfb1cdcab2
natanael <nathaxx09@hotmail.com>
2025-03-04 01:30:45 -0300

    cambio de formato

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ import './menu-movil.js';
 const text = document.getElementById('myText');
 
 gsap.to(text, {
-    duration: 2.3,
+    duration: 1.5,
+    ease: "none",
+    text: "Web Developer",
+});
+gsap.to(text, {
+    delay: 3,
+    duration: 1.5,
+    text: "Desenvolvedor web",
+    ease: "none",
+});
+gsap.to(text, {
+    delay: 6,
+    duration: 1.5,
     text: "Desarrollador Web",
     ease: "none",
 });
